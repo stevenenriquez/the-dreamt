1e1ca52bf20bb3c00a2a859cb254f6cb04d8190b
stevenenriquez <stevenenriquez123@gmail.com>
2020-11-25 17:53:59 -0700

    spring initalizr

---
+++ the-dreamt-api/src/main/java/com/thedreamt/thedreamtapi/TheDreamtApiApplication.java
@@ -0,0 +1,13 @@
+package com.thedreamt.thedreamtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDreamtApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDreamtApiApplication.class, args);
+	}
+
+}
---
+++ the-dreamt-api/src/test/java/com/thedreamt/thedreamtapi/TheDreamtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedreamt.thedreamtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDreamtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
